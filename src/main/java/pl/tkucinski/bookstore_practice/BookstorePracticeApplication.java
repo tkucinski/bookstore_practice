37e199fe8ea631dd01c57093a7a4df54aeca80b8
TK <tomekkucinski@gmail.com>
2025-05-07 21:54:09 +0200

    initial commit

---
+++ src/main/java/pl/tkucinski/bookstore_practice/BookstorePracticeApplication.java
@@ -0,0 +1,13 @@
+package pl.tkucinski.bookstore_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstorePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstorePracticeApplication.class, args);
+	}
+
+}
